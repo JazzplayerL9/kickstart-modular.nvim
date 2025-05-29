3a1d6ebac3a9122398d955ebf756adc69cc8020f
Zach Jones <jazzplayerl9@gmail.com>
2025-05-28 22:08:41 -0600

    Updated options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -49,7 +49,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
