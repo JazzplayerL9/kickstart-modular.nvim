b02beb8bff57a8b414d4d832560b76f6b37b4c01
Zach Jones <JazzplayerL9@gmail.com>
2026-04-17 17:03:50 -0600

    Added flash.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -135,7 +135,7 @@ return {
     opts = {},
     keys = {
       {
-        '<leader>xx',
+        '<leader>q',
         '<cmd>Trouble diagnostics toggle<cr>',
         desc = 'Diagnostics (Trouble)',
       },
@@ -164,6 +164,20 @@ return {
         '<cmd>Trouble qflist toggle<cr>',
         desc = 'Quickfix List (Trouble)',
       },
+    },
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
   {
