1f7297972b09894181b3213ab6c97d39322a0965
Zach Jones <jazzplayerl9@gmail.com>
2026-05-09 10:51:54 -0500

    Fixed some warnings in checkhealth and added neo-tree optional components for funsies.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,6 +8,50 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+          -- Custom filter function to handle the case where no windows are left
+          filter_func = function(window_ids, filters)
+            local default_filter = require('window-picker.filters.default-window-filter'):new()
+            default_filter:set_config(filters)
+            local filtered = default_filter:filter_windows(window_ids)
+
+            if #filtered == 0 then
+              -- Fallback: If everything was filtered out, just return the current window
+              return { vim.api.nvim_get_current_win() }
+            end
+
+            return filtered
+          end,
+        }
+      end,
+    },
+    {
+      'antosha417/nvim-lsp-file-operations',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        require('lsp-file-operations').setup()
+      end,
+    },
+    {
+      'folke/snacks.nvim',
+      opts = {
+        image = { enabled = true },
+      },
+    },
   },
   lazy = false,
   keys = {
@@ -27,6 +71,20 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['<cr>'] = function(state)
+            local node = state.tree:get_node()
+            if node.type == 'file' then
+              local winid = require('window-picker').pick_window()
+              if winid then
+                vim.api.nvim_set_current_win(winid)
+                require('neo-tree.sources.filesystem.commands').open(state)
+              end
+            else
+              require('neo-tree.sources.filesystem.commands').open(state)
+            end
+          end,
+          ['s'] = 'split_with_window_picker',
+          ['v'] = 'vsplit_with_window_picker',
         },
       },
     },
